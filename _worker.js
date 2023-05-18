3920e05ec719c02e3092178a0b1cef49076adbf9
xingziyang <ziyanghenan@126.com>
2023-05-18 11:59:21 +0800

    Update _worker.js
    修改脚本

--- _worker.js
+++ _worker.js
@@ -1,21 +1,13 @@
-const TELEGRAPH_URL = 'https://api.openai.com';
-
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-
-async function handleRequest(request) {
-  const url = new URL(request.url);
-  const headers_Origin = request.headers.get("Access-Control-Allow-Origin") || "*"
-  url.host = TELEGRAPH_URL.replace(/^https?:\/\//, '');
-  const modifiedRequest = new Request(url.toString(), {
-    headers: request.headers,
-    method: request.method,
-    body: request.body,
-    redirect: 'follow'
-  });
-  const response = await fetch(modifiedRequest);
-  const modifiedResponse = new Response(response.body, response);
-  modifiedResponse.headers.set('Access-Control-Allow-Origin', headers_Origin);
-  return modifiedResponse;
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    url.host = "https://api.openai.com/";
+    // openai is already set all CORS heasders 
+    return fetch(url, {
+      headers: request.headers,
+      method: request.method,
+      body: request.body,
+      redirect: 'follow'
+    });
+  }
 }
