8e15418316173fabf8dbe97cd70379cb707f7f41
xingziyang <ziyanghenan@126.com>
2023-05-18 18:11:06 +0800

    Update _worker.js
    修改脚本

--- _worker.js
+++ _worker.js
@@ -1,11 +1,28 @@
+
+const TELEGRAPH_URL = 'https://api.openai.com';
+
+
 export default {
   async fetch(request, env) {
-    let url = new URL(request.url);
-    if (url.pathname.startsWith('/')) {
-      url.hostname = 'api.openai.com'
-      let new_request = new Request(url, request);
-      return fetch(new_request);
-    }
-    return env.ASSETS.fetch(request);
+      const NewResponse = await handleRequest(request)
+      return NewResponse
   },
+
 };
+
+async function handleRequest(request) {
+  const url = new URL(request.url);
+  const headers_Origin = request.headers.get("Access-Control-Allow-Origin") || "*"
+  url.host = TELEGRAPH_URL.replace(/^https?:\/\//, '');
+  const modifiedRequest = new Request(url.toString(), {
+    headers: request.headers,
+    method: request.method,
+    body: request.body,
+    redirect: 'follow'
+  });
+  const response = await fetch(modifiedRequest);
+  const modifiedResponse = new Response(response.body, response);
+  // 添加允许跨域访问的响应头
+  modifiedResponse.headers.set('Access-Control-Allow-Origin', headers_Origin);
+  return modifiedResponse;
+}
